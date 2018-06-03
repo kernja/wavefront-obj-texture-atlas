629b5b4ff8f5c15dc9b0a8c1c6114b95e08d042e
Jeffrey Kern <anwserman@yahoo.com>
2018-06-03 16:39:36 -0400

    added mssing files
    added files missing from original gitignore

---
+++ ObjAtlas/ObjAtlas/WaveFront/Obj/FaceVertexData.cs
@@ -0,0 +1,119 @@
+﻿using ObjAtlas.Extensions;
+using ObjAtlas.WaveFront.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjAtlas.WaveFront.Obj
+{
+    [Serializable]
+    public class FaceVertexData
+    {
+        private int _vIndex = 0;
+        private int? _vTextureIndex = null;
+        private int? _vNormalIndex = null;
+
+        private Vertex _vertex { get; set; }
+        private UV _texture { get; set; }
+        private Vertex _normal { get; set; }
+
+        public FaceVertexData()
+        {
+            _vertex = new Vertex(0, 0, 0);
+            _texture = null;
+            _normal = null;
+        }
+        public FaceVertexData(string parseData)
+        {
+            //clean up the string
+            parseData = parseData.Trim();
+            parseData = parseData.RemoveSpaces();
+
+            //get the data
+            if (parseData.Contains("/"))
+            {
+                string[] sSplit = parseData.Split('/');
+                if (sSplit.Length == 2)
+                {
+                    _vIndex = int.Parse(sSplit[0]);
+                    _vTextureIndex = (!string.IsNullOrEmpty(sSplit[1]) ? int.Parse(sSplit[1]) : (int?)null);
+                }
+                else if (sSplit.Length == 3)
+                {
+                    _vIndex = int.Parse(sSplit[0]);
+                    _vTextureIndex = (!string.IsNullOrEmpty(sSplit[1]) ? int.Parse(sSplit[1]) : (int?)null);
+                    _vNormalIndex = (!string.IsNullOrEmpty(sSplit[2]) ? int.Parse(sSplit[2]) : (int?)null);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Invalid file data");
+                }
+            }
+            else
+            {
+                _vIndex = int.Parse(parseData);
+            }
+        }
+
+        public void LoadDataFromFactories(DataFactory<Vertex> pVertexFactory, DataFactory<UV> pTextureFactory, DataFactory<Vertex> pNormalFactory)
+        {
+            _vertex = pVertexFactory.ObjectAtIndex(_vIndex);
+
+            if (_vTextureIndex.HasValue)
+                _texture = pTextureFactory.ObjectAtIndex(_vTextureIndex.Value);
+
+            if (_vNormalIndex.HasValue)
+                _normal = pNormalFactory.ObjectAtIndex(_vNormalIndex.Value);
+        }
+
+        public Vertex GetVertex()
+        {
+            return _vertex;
+        }
+
+        public UV GetTextureUV()
+        {
+            return _texture;
+        }
+
+        public Vertex GetNormals()
+        {
+            return _normal;
+        }
+
+        public void SetVertex(float pX, float pY, float pZ, float? pW = null)
+        {
+            _vertex = new Vertex(pX, pY, pZ, pW);
+        }
+        public void SetNormal(float pX, float pY, float pZ, float? pW = null)
+        {
+            _normal = new Vertex(pX, pY, pZ, pW);
+        }
+        public void SetTextureUV(float pU, float pV, float? pW = null)
+        {
+            _texture = new UV(pU, pV, pW);
+        }
+
+        public override string ToString()
+        {
+            return "FaceVertexData";
+            /*
+            if (_vTextureIndex == -1 && _vNormalIndex == -1)
+            {
+                return string.Format("{0}", _vIndex.ToString());
+            } else if (_vTextureIndex != -1 && _vNormalIndex == -1)
+            {
+                return string.Format("{0}/{1}", _vIndex.ToString(), _vTextureIndex.ToString());
+            } else if (_vTextureIndex == -1 && _vNormalIndex != -1)
+            {
+                    return string.Format("{0}//{1}", _vIndex.ToString(), _vNormalIndex.ToString());
+            } else
+            {
+                    return string.Format("{0}/{1}/{2}", _vIndex.ToString(),  _vTextureIndex.ToString(), _vNormalIndex.ToString());
+       
+            }*/
+        }
+    }
+}
---
+++ ObjAtlas/ObjAtlas/WaveFront/Obj/Group.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjAtlas.WaveFront.Obj
+{
+    [Serializable]
+    public class Group
+    {
+        public string groupName { get; set; }
+        public string materialName { get; set; }
+        private IList<Face> _faces;
+
+        public Group(string pName = "")
+        {
+            groupName = pName;
+            materialName = "";
+            _faces = new List<Face>();
+        }
+
+        public void AddFace(string parseData)
+        {
+            _faces.Add(new Face(parseData));
+        }
+
+        public List<Face> GetFaces()
+        {
+            return _faces.ToList();
+        }
+
+        public PointF GetUVMin()
+        {
+            float uMin = float.MaxValue;
+            float vMin = float.MaxValue;
+
+            foreach (var f in _faces)
+            {
+                foreach (var fv in f.GetVertices())
+                {
+                    var uv = fv.GetTextureUV();
+                    if (uv != null)
+                    {
+                        if (uv.X < uMin)
+                            uMin = uv.X;
+
+                        if (uv.Y < vMin)
+                            vMin = uv.Y;
+
+                    }
+                }
+            }
+
+            return new PointF(uMin, vMin);
+        }
+
+        public PointF GetUVMax()
+        {
+            float vMax = float.MinValue;
+            float uMax = float.MinValue;
+
+            foreach (var f in _faces)
+            {
+                foreach (var fv in f.GetVertices())
+                {
+                    var uv = fv.GetTextureUV();
+                    if (uv != null)
+                    {
+                        if (uv.X > uMax)
+                            uMax = uv.X;
+
+                        if (uv.Y > vMax)
+                            vMax = uv.Y;
+                    }
+                }
+            }
+
+            return new PointF(uMax, vMax);
+        }
+    }
+}
